72f2d35b066528463b55da170c3f9c6b7f39f6e6
tdds <tdds@localhost.localdomain>
2021-04-05 05:06:44 +0700

    Migration

--- app.rb
+++ app.rb
@@ -10,5 +10,5 @@ class Client < ActiveRecord::Base
 end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>!!!!!"			
 end
