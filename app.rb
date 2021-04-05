541dffee47f830c85642b234b70b29a5c0fa6dc5
tdds <tdds@localhost.localdomain>
2021-04-06 06:47:13 +0700

    On homework

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+
+
+
 set :database, { adapter: 'sqlite3', database: 'barbershop.db' }
 
 class Client < ActiveRecord::Base 
@@ -12,7 +15,30 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
+before do 
 	@barbers = Barber.order "created_at DESC"
+end
+
+
+get '/' do
+	
 	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:date]
+	@barbers = params[:barbers]
+	@color = params[:color]
+	
+
+	
+
+	erb :visit
+		
 end
