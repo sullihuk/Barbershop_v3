9ca99dbe51a0a9567581ed5a0d4b4fb79cf84468
sullihuk <tdds@mx>
2021-04-12 07:43:38 +0700

    Finish

--- app.rb
+++ app.rb
@@ -92,3 +92,14 @@ get "/barber/:id" do
 	@barber = Barber.find params[:id]
 	erb :barber
 end
+
+get '/bookings' do
+	@clients =Client.order("created_at DESC")
+	erb :bookings
+end
+
+get "/client/:id" do
+	@client = Client.find params[:id]
+	erb :client
+
+	end
